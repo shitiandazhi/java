3225694ec70892d86d37dcfecc1c4734dbcb1a52
shiti <shitiandazhi163@gmail.com>
2023-10-06 17:26:04 +0900

    practice5

---
+++ src/chapter2/c2_5.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+public class c2_5 {
+
+	public static void main(String[] args) {
+       
+		String str = """
+			   おはよう
+			   こんにちは
+			   こんばんわ
+				""";
+		System.out.println(str);
+	}
+
+}
