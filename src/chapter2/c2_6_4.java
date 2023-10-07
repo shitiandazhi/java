0547ca18cf128b6a76b8e27a2ec0c567a0e566c9
shiti <shitiandazhi163@gmail.com>
2023-10-07 23:59:12 +0900

    practice7

---
+++ src/chapter2/c2_6_4.java
@@ -0,0 +1,11 @@
+package chapter2;
+
+public class c2_6_4 {
+
+	public static void main(String[] args) {
+      double ans = 55.2;
+      String msg = "答えは"+ ans +"です";
+      System.out.println(msg);
+	}
+
+}
---
+++ src/chapter2_7/c1.java
@@ -0,0 +1,14 @@
+package chapter2_7;
+
+public class c1 {
+
+	public static void main(String[] args) {
+      
+	   double ans1 = Math.sqrt(20.5);
+       System.out.println("平方根=" + ans1);
+       
+       double ans2 = Math.pow(2, 10);
+       System.out.println("平方根=" + ans1);
+	}
+
+}
