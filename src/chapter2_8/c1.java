e445b3eb22036c4bece82c9fd2bb31ac4f7774cf
shiti <shitiandazhi163@gmail.com>
2023-10-10 00:17:26 +0900

    practice9

--- src/chapter2_8/c1.java
+++ src/chapter2_8/c1.java
@@ -1,9 +1,12 @@
 package chapter2_8;
 
+import jp.kwebs.lib.Input;
+
 public class c1 {
 
 	public static void main(String[] args) {
-     
+     double value = Input.getDouble();
+     System.out.println("平方根=" + Math.sqrt(value));
 	}
 
 }
