c333945aa6383371b39c143c0dfae0ca16573c32
shiti <shitiandazhi163@gmail.com>
2023-10-11 23:49:05 +0900

    practice12

---
+++ src/chapter3_1/c1.java
@@ -0,0 +1,14 @@
+package chapter3_1;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		
+		int[] number = {5, 12, 8, 21, 9};
+		
+		for(int n : number) {
+			System.out.print(n + "\t");
+		}
+	}
+
+}
