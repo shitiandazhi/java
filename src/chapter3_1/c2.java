83c26706fea036aa8063fe563c8bcb84eafdd87e
shiti <shitiandazhi163@gmail.com>
2023-10-12 22:25:48 +0900

    practice12

---
+++ src/chapter3_1/c2.java
@@ -0,0 +1,18 @@
+package chapter3_1;
+//合計と平均
+public class c2 {
+
+	public static void main(String[] args) {
+		
+		int[] number = {5, 12, 8, 21, 9};
+		int total = 0;
+		
+		for(int n : number) {
+			total += n;
+		}
+		
+		System.out.println("合計=" + total);
+		System.out.println("平均=" + total/number.length);
+	}
+
+}
