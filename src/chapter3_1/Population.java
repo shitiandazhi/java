2a30751604441e4814b935473e1095d20de9c099
shiti <shitiandazhi163@gmail.com>
2023-10-12 23:46:22 +0900

    practice13

---
+++ src/chapter3_1/Population.java
@@ -0,0 +1,3 @@
+package chapter3_1;
+
+public record Population(String prefecture, int population, double rate) {}
---
+++ src/chapter3_1/c3.java
@@ -0,0 +1,13 @@
+package chapter3_1;
+
+public class c3 {
+
+    public static void main(String[] args) {
+        Population p = new Population("東京都", 13921, 7.1);
+
+        System.out.println(p.prefecture());
+        System.out.println(p.population());
+        System.out.println(p.rate());
+    }
+
+}
