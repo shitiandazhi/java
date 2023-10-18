aa4d65c1ff51a2b608ddd2d4ec23f9cef111616e
shiti <shitiandazhi163@gmail.com>
2023-10-18 19:25:45 +0900

    practice20

---
+++ src/chapter4_1/c1.java
@@ -0,0 +1,15 @@
+package chapter4_1;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		System.out.println("Start");
+		message();
+		System.out.println("end");
+	}
+	
+	public static void message() {
+		System.out.println("Hello!");
+	}
+
+}
