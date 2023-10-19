314591c0a1ad8c703b6479efa9cddbfe343a1b7c
shiti <shitiandazhi163@gmail.com>
2023-10-19 23:59:54 +0900

    practice21

---
+++ src/chapter4_1/Util.java
@@ -0,0 +1,14 @@
+package chapter4_1;
+
+public class Util {
+
+	public static double menseki (double height, double width) {
+		double s = height * width;
+		return s;
+	}
+
+	public static double menseki (double hankei) {
+		double s = Math.pow(hankei,2) * Math.PI;
+		return s;
+	}
+}
---
+++ src/chapter4_1/c7.java
@@ -0,0 +1,9 @@
+package chapter4_1;
+
+public class c7 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
