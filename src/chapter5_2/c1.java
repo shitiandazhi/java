22e9ea9b4d4ed7c4c900bfca2c1c77b98deffd3d
shiti <shitiandazhi163@gmail.com>
2023-10-23 23:58:35 +0900

    prctice25

---
+++ src/chapter5_2/c1.java
@@ -0,0 +1,12 @@
+package chapter5_2;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		double value = Math.random();
+		if(value>=0.9) {
+			System.out.println("大吉");
+		}
+	}
+
+}
