4cf5e265c060a43653e51ee7e51efebd209f614f
shiti <shitiandazhi163@gmail.com>
2023-10-25 22:48:49 +0900

    practice28

---
+++ src/chapter5_4/c1.java
@@ -0,0 +1,12 @@
+package chapter5_4;
+
+public class c1 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Hello!");
+		}
+	}
+
+}
---
+++ src/chapter5_4/c2.java
@@ -0,0 +1,11 @@
+package chapter5_4;
+
+public class c2 {
+
+	public static void main(String[] args) {
+		for (int i = 0; i<3; i++) {
+			System.out.println("i=" + i);
+		}
+	}
+
+}
