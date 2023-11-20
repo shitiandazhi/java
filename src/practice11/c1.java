332fa4c49fee3e1fd4690a25d0a0935ee7abaeda
shiti <shitiandazhi163@gmail.com>
2023-11-20 23:19:35 +0900

    practice48

--- src/practice11/c1.java
+++ src/practice11/c1.java
@@ -7,6 +7,7 @@ public class c1 {
 		 System.out.println("引数は" + args.length + "個指定されています。");
 	        for(int idx = 0 ; idx < args.length; idx ++){
 	            System.out.println((idx + 1 ) + "番目の引数は「" + args[idx] + "」です。");
+	            
 	        }
 	}
 
