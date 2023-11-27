6c326521da8f4cea4a0d2d4f22b8dfd9a8109f8a
shiti <shitiandazhi163@gmail.com>
2023-11-27 23:41:23 +0900

    practice50

---
+++ src/practice11/c3.java
@@ -0,0 +1,18 @@
+package practice11;
+
+public class c3 {
+
+	public static void main(String[] args) {
+		
+		{
+	        int x;
+	        int y;
+
+	        x = 13;
+	        y = 17;
+
+	        System.out.println( "x=" + x + ",y=" + y );
+		}
+	}
+
+}
