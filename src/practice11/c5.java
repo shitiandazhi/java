88e1a658cc75f32f863e65092d6214b787e7e3f4
shiti <shitiandazhi163@gmail.com>
2023-11-29 21:52:54 +0900

    practice53

---
+++ src/practice11/c5.java
@@ -0,0 +1,14 @@
+package practice11;
+
+public class c5 {
+
+	public static void main(String[] args) {
+		
+		int x = 12;
+
+        System.out.println( x * 2 );
+        System.out.println( x * 3 );
+        System.out.println( x * 4 );
+	}
+
+}
